8fb2c4ce8b0f6893008269923d4e16d2d0551ef7
razikhaNawaz <razikhafaheem02@gmail.com>
2023-10-17 10:54:07 +0530

    completed

--- src/app/Components/Layout/Section5.js
+++ src/app/Components/Layout/Section5.js
@@ -4,8 +4,7 @@ import classes from "./Section5.module.css";
 const Section5 = () => {
   return (
     <div className={classes.parent}>
-      <h1>Ready to Cash in Your Connections?
-Join Our Affiliate Program</h1>
+      <h1>Ready to Cash in Your Connections?Join Our Affiliate Program</h1>
 <p>Join now and get <span>7.5%</span> commission on all creator referrals for the entire year.</p>
 
 
